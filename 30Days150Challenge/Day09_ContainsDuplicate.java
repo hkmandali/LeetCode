d83856b6d97c483d713faefc9ce562012f5b16fe
hkmandali <mandalihemanthkrishna@gmail.com>
2022-02-06 15:56:10 -0500

    New: Added Day 09 of 150 challenge contains duplicate

---
+++ 30Days150Challenge/Day09_ContainsDuplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class Day09_ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i:nums)
+        {
+            if(hs.contains(i))
+                return true;
+            hs.add(i);
+        }
+        return false;
+    }
+}
