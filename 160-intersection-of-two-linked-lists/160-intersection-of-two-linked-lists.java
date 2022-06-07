1a8a595466143ac651e23e880180ed891aff5407
Hemanth Mandali <55756253+hkmandali@users.noreply.github.com>
2022-06-06 20:46:55 -0700

    Time: 1 ms (99.19%), Space: 55.3 MB (48.62%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -13,6 +13,7 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if(null == headA || null == headB)
             return null;
+        /*
         HashSet<ListNode> hs = new HashSet<>();
         ListNode iter = headA;
         while(iter!= null)
@@ -30,6 +31,25 @@ public class Solution {
             iter = iter.next;
         }
         return null;
-        
+        */
+        // single pass, checked from discuss section
+        // as the last nodes after the intersection in both the lists are the same, we can iterate both the lists i.e append list A to list B , similarly
+        // list B to list A, once the values are equal, we can stop iterating 
+        ListNode itrA = headA , itrB = headB;
+        //System.out.println("value of A and B is "+itrA.val+"   "+itrB.val);
+        while(itrA != itrB)
+        {
+            if(itrA == null)
+                itrA = headB;
+            else
+                itrA = itrA.next;
+            if(itrB == null)
+                itrB = headA;
+            else
+                itrB = itrB.next;
+            
+            
+        }
+        return itrA;
     }
 }
